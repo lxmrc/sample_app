6224d2c1424641464906e86c2c15a9d2391e91a4
Alex <alex.mercea@protonmail.com>
2020-12-18 12:00:16 +1100

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'polar-reef-37954.herokuapp.com'
+  host = 'rails-tutorial-lxmrc.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
